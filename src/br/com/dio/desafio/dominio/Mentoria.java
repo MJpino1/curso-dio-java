65fbcd25e25f04845fd4a02b4c367b7384f02440
mikel <mikeljcp@gmail.com>
2022-11-06 13:10:35 -0300

    commit 3 praticando curso

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -6,6 +6,11 @@ public class Mentoria {
 	private String titulo;
 	private String descricao;
 	private LocalDate data;
+	
+	public Mentoria() {
+		
+	}
+	
 	public String getTitulo() {
 		return titulo;
 	}
