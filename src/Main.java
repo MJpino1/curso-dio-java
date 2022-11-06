c497719a2c4283062fee890d02c5493936f9d208
mikel <mikeljcp@gmail.com>
2022-11-06 12:23:00 -0300

    Segundo commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import br.com.dio.desafio.dominio.Curso;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Curso curso1 = new Curso();
+		
+		System.out.println(curso1);
+	}
+
+}
